d1b954a89d3b3c97c647f1586eaf14e6c46b0c5f
bugraaldal <61989756+bugraaldal@users.noreply.github.com>
2020-12-16 14:34:52 +0300

    Create Day3.py

---
+++ Day3/Day3.py
@@ -0,0 +1,27 @@
+with open("Day3.txt", "r") as f:
+    input = f.read().split("\n")
+
+
+def Route(X, Y, grid):
+    trees = 0
+    x = 0
+    for row in grid[::Y]:
+        if row[x] == '#':
+            trees += 1
+
+        for i in range(X):
+            x += 1
+            if x == len(row):
+                x = 0
+    return trees
+
+
+tests = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+# Part 2
+total = 1
+for test in tests:
+    total = total * Route(test[0], test[1], input)
+
+print(Route(3, 1, input))  # Part 1 answer
+print(total)  # Part 2 answer
