7929c93f24c96301a1f1204ad9de427fca1556b7
bugraaldal <61989756+bugraaldal@users.noreply.github.com>
2020-12-16 11:09:12 +0300

    Update Day2.py

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,5 +1,5 @@
 """ There was no need for this much lists. It could have been done with less and the code would be clearer, however this whole
-thinng is about seeing my growth and mistakes. Therefore I'm keeping the lists. """
+thinng is about seeing my growth and mistakes. Therefore I'm keeping it """
 InputList = []
 new_list = []
 final_list = []
@@ -10,7 +10,7 @@ Words = []
 with open("Day2.txt", "r") as f:
     for line in f:
         InputList.append(line)
-# Setting up the lists -could have been avoided/used less throwaway lists.
+# Setting up the lists
 for ch in InputList:
     new = ch.replace("\n", "")
     new_list.append(new)
@@ -18,7 +18,7 @@ for i in new_list:
     final_list.append(i.split(":"))
 for each in final_list:
     wordList.append(each[0].split())
-# Seperating lists into three parts, range, character and the password.
+# Seperating lists into three parts, range, character and the password
 for eachOne in wordList:
     RangeList.append(eachOne[0].split("-"))
 for eachItem in final_list:
@@ -26,20 +26,15 @@ for eachItem in final_list:
 for k in wordList:
     Words.append(k[1])
 
-# Part 1
-valid = 0
+valid = 0  # Part 1 answer
+x = 0  # Part 2 answer
 for password, word, num in zip(Passwords, Words, RangeList):
     lil = int(num[0])
     big = int(num[1])
     if password.count(word) <= big and password.count(word) >= lil:
         valid += 1
-print(valid)
-
-# Part 2
-x = 0
-for password, word, num in zip(Passwords, Words, RangeList):
-    lil = int(num[0])
-    big = int(num[1])
     if (password[lil] == word or password[big] == word) and password[lil] != password[big]:
         x += 1
+print(valid)
 print(x)
+
